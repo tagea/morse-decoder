26f95e20ac6f78281010495f4d62a1d60ae31436
Yulya Vasilyeva <043@tut.by>
2019-09-23 18:16:32 +0300

    feat: create task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let resultAlpha = '';
+    let alphaStr = '';
+    for(let i = 0; i < expr.length; i += 10){
+        alphaStr  = expr.substring(i,i+10);
+        resultAlpha = '';
+        
+        for(let j = 0; j < alphaStr.length; j += 2){
+            let alpha = alphaStr.substring(j,j+2);
+            
+            if(alpha === '10'){
+                resultAlpha += '.';
+            }
+            if(alpha === '11'){
+                resultAlpha += '-'; 
+            }
+            if(alpha.indexOf('*') !== -1){
+                resultAlpha = 'space';
+                break;
+            }
+        }
+        if(resultAlpha === 'space'){
+            result += ' ';
+        }else{
+            result += MORSE_TABLE[resultAlpha];
+        }
+    }
+    return result;
 }
 
 module.exports = {
